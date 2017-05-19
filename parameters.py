40cc682ba8e8bfb540f6bc0e8200bbd4dbebeb7e
Hang Wu <wuhang0613@gmail.com>
2017-05-18 22:49:46 -0400

    mr

--- parameters.py
+++ parameters.py
@@ -1,4 +1,4 @@
-redisHost = 'ec2-54-175-141-176.compute-1.amazonaws.com'
+redisHost = 'ec2-52-205-251-116.compute-1.amazonaws.com'
 redisPort = 6380
 NEWS_TABLE_NAME = "newCollection"
 CLICK_LOGS_TABLE_NAME = 'click_logs'
@@ -9,12 +9,12 @@ NEWS_TIME_OUT_IN_SECONDS = 3600 * 24
 SLEEP_TIME_IN_SECONDS = 10
 
 ## Kafka Topic
-DEDUPE_NEWS_TASK_QUEUE = 'DEDUPE_NEWS_TASK_QUEUE'
-SCRAPE_NEWS_TASK_QUEUE = 'SCRAPE_NEWS_TASK_QUEUE'
-LOG_CLICKS_TASK_QUEUE = 'LOG_CLICKS_TASK_QUEUE'
+DEDUPE_NEWS_TASK_QUEUE = 'DEDUPE'
+SCRAPE_NEWS_TASK_QUEUE = 'SCRAPE'
+LOG_CLICKS_TASK_QUEUE = 'LOG'
 
 Service_SERVER_PORT = 4040
-KAFKA_SERVER = ['ec2-54-175-141-176.compute-1.amazonaws.com:9092', 'ec2-34-207-91-169.compute-1.amazonaws.com:9092']
+KAFKA_SERVER = ['ec2-54-163-153-215.compute-1.amazonaws.com:9092', 'ec2-52-207-212-20.compute-1.amazonaws.com:9092']
 
 PREFERENCE_MODEL_TABLE_NAME = "user_preference_model"
 Recommendation_SERVER_PORT = 5050
@@ -23,6 +23,6 @@ SAME_NEWS_SIMILARITY_THRESHOLD = 0.9
 
 SERVER_HOST = 'ec2-107-23-249-234.compute-1.amazonaws.com'
 
-MONGO_DB_HOST = 'ec2-34-207-91-169.compute-1.amazonaws.com'
+MONGO_DB_HOST = 'ec2-52-205-251-116.compute-1.amazonaws.com'
 MONGO_DB_PORT = 33333
 DB_NAME = 'RecommendationNews'
